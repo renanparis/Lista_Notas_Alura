e8b563ac12245810b1ee87bc4ce7fdcd35564982
renanparis <paris.oliveira@gmail.com>
2019-05-02 18:07:19 -0300

    Teste performance quantidade de itens em uma lista.

--- app/src/main/java/com/renanparis/ceed/ui/activity/ListNotesActivity.java
+++ app/src/main/java/com/renanparis/ceed/ui/activity/ListNotesActivity.java
@@ -3,8 +3,14 @@ package com.renanparis.ceed.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.renanparis.ceed.R;
+import com.renanparis.ceed.dao.NoteDao;
+import com.renanparis.ceed.model.Note;
+import com.renanparis.ceed.ui.adapter.LitsAdapterNotes;
+
+import java.util.List;
 
 public class ListNotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,16 @@ public class ListNotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notes);
+
+        ListView listNotes = findViewById(R.id.activity_list_notes_listView);
+
+        NoteDao dao = new NoteDao();
+        for (int i = 1; i <= 10000; i++) {
+            dao.insert(new Note("Titulo" + 1, "Descrição" + i));
+        }
+        List<Note> list = dao.todos();
+        listNotes.setAdapter(new LitsAdapterNotes(this, list));
+
+
     }
 }
