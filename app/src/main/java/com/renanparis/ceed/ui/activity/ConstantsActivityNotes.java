c9cb7e7347d458c1f1fa40504958e26b746d0eff
renanparis <paris.oliveira@gmail.com>
2019-05-06 21:34:49 -0300

    Adição da Interface com constantes das activities

---
+++ app/src/main/java/com/renanparis/ceed/ui/activity/ConstantsActivityNotes.java
@@ -0,0 +1,8 @@
+package com.renanparis.ceed.ui.activity;
+
+public interface ConstantsActivityNotes {
+
+    String KEY_NOTE = "note";
+    int RESULT_CODE_CREATED_NOTE = 2;
+    int REQUEST_CODE_INSERT_NOTE = 1;
+}
