90b6e48de53c71d6ec9480ded02090acdbbe77e9
renanparis <paris.oliveira@gmail.com>
2019-06-26 17:24:51 -0300

    Adição da activity e layout feedback.

---
+++ app/src/main/java/com/renanparis/ceed/ui/activity/FeedBackActivity.java
@@ -0,0 +1,16 @@
+package com.renanparis.ceed.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.renanparis.ceed.R;
+
+public class FeedBackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back);
+    }
+}
